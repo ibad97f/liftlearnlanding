1236d116728d580445650a4fa6bc27f873acabea
Bilal raza <attari1235@gmail.com>
2024-05-10 15:54:46 +0500

    terms updated

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -38,8 +38,34 @@ function TermsAndConditions() {
       </section>
 
       <section className="my-8">
+        <h2 className="text-2xl font-semibold mb-2">4. Subscriptions</h2>
+        <p>
+          We offer two types of subscriptions: Full Subscription and Exercise
+          Videos Subscription. By subscribing to our services, you agree to pay
+          the applicable fees and abide by the terms and conditions outlined
+          herein.
+        </p>
+        <p>
+          a. <strong>Full Subscription:</strong> This subscription provides
+          access to all features and content available in the app.
+        </p>
+        <p>
+          b. <strong>Exercise Videos Subscription:</strong> This subscription
+          provides access to our library of exercise videos.
+        </p>
+        <p>
+          All subscription purchases made through Apple's App Store are subject
+          to Apple's terms and conditions. Payment will be charged to your Apple
+          ID account at the confirmation of purchase. Subscriptions
+          automatically renew unless canceled at least 24 hours before the end
+          of the current period. You can manage and cancel your subscriptions by
+          going to your Account Settings on the App Store after purchase.
+        </p>
+      </section>
+
+      <section className="my-8">
         <h2 className="text-2xl font-semibold mb-2">
-          4. Intellectual Property
+          5. Intellectual Property
         </h2>
         <p>
           All content provided in the app, including but not limited to text,
@@ -50,7 +76,7 @@ function TermsAndConditions() {
 
       <section className="my-8">
         <h2 className="text-2xl font-semibold mb-2">
-          5. Limitation of Liability
+          6. Limitation of Liability
         </h2>
         <p>
           We shall not be liable for any direct, indirect, incidental, special,
@@ -60,13 +86,16 @@ function TermsAndConditions() {
       </section>
 
       <section className="my-8">
-        <h2 className="text-2xl font-semibold mb-2">6. Governing Law</h2>
-        <p>These terms and conditions shall be governed by and construed in accordance with the laws of the United States of America, without regard to its conflict of law provisions.</p>
-
+        <h2 className="text-2xl font-semibold mb-2">7. Governing Law</h2>
+        <p>
+          These terms and conditions shall be governed by and construed in
+          accordance with the laws of the United States of America, without
+          regard to its conflict of law provisions.
+        </p>
       </section>
 
       <section className="my-8">
-        <h2 className="text-2xl font-semibold mb-2">7. Contact Us</h2>
+        <h2 className="text-2xl font-semibold mb-2">8. Contact Us</h2>
         <p>
           If you have any questions or concerns about these terms and
           conditions, you may contact us at:
@@ -77,8 +106,8 @@ function TermsAndConditions() {
 
       <footer className="my-8">
         <p>
-          These terms and conditions are effective as of 5-May-2024. Any updates or
-          changes to our terms will be posted on this page.
+          These terms and conditions are effective as of 5-May-2024. Any updates
+          or changes to our terms will be posted on this page.
         </p>
       </footer>
     </div>
